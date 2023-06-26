fb9e25294f7cf2dd4e3a216b58d6513b97adbcd9
Yasemin-s <l2012721036@isparta.edu.tr>
2023-06-26 22:05:22 +0300

    .
    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,103 @@
+// import 'dart:convert';
+
+// import 'package:fake_backend/model/loadData.dart';
+// import 'package:flutter/material.dart';
+// import "package:http/http.dart" as http;
+
+// void main() {
+//   runApp(const MyApp());
+// }
+// //apiyi calistirmak icinn metod yazmamiz lazim
+// Future<LoadData> apiCall() async {
+//   String urlString = "http://localhost:3000/db";
+//   Uri uri = Uri.parse(urlString);
+//   final response = await http.get(uri);
+//   if(response.statusCode == 200){
+//     return LoadData.fromJson(json.decode(response.body));
+//   } else{
+//     print('Hata Kodu: ${response.statusCode}');
+//     throw Exception("Bir hata oluştu");
+//   }
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Program API',
+//       theme: ThemeData(
+//       visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: const MyHomePage(title: 'Program API'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     return Scaffold(
+//     body: FutureBuilder<LoadData>(
+//       future: apiCall(),
+//       builder: (context, snapshot){
+//         //data null ise
+//         if(snapshot.hasData){ //bos degilse dedik
+//           return Container(
+//             child: Center(
+//               child: Text(
+//                 "gun : ${snapshot.data?.gun} \n  sabah : ${snapshot.data?.sabah} \n  ogle : ${snapshot.data?.ogle} \n  araogun : ${snapshot.data?.araogun} \n  aksam : ${snapshot.data?.aksam}"
+//               ),
+//             ),
+//           );
+//         } else {
+//           return Center(child: CircularProgressIndicator(),);
+//         }
+//       },
+//     )
+//     );
+//   }
+// }
+
 import 'dart:convert';
 
-import 'package:fake_backend/model/loadData.dart';
 import 'package:flutter/material.dart';
-import "package:http/http.dart" as http;
+import 'package:http/http.dart' as http;
+
+import 'model/loadData.dart';
 
 void main() {
   runApp(const MyApp());
 }
-//apiyi calistirmak icinn metod yazmamiz lazim
+
 Future<LoadData> apiCall() async {
   String urlString = "http://localhost:3000/db";
   Uri uri = Uri.parse(urlString);
   final response = await http.get(uri);
-  if(response == 200){
+  if (response.statusCode == 200) {
     return LoadData.fromJson(json.decode(response.body));
-  } else{
-    throw Exception("Bir hata olutu");
+  } else {
+    print('Hata Kodu: ${response.statusCode}');
+    throw Exception("Bir hata oluştu");
   }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +105,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Program API',
       theme: ThemeData(
-      visualDensity: VisualDensity.adaptivePlatformDensity,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'Program API'),
     );
@@ -36,7 +113,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -45,29 +122,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<LoadData> _loadDataFuture;
 
+  @override
+  void initState() {
+    super.initState();
+    _loadDataFuture = apiCall();
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-    body: FutureBuilder<LoadData>(
-      future: apiCall(),
-      builder: (context, snapshot){
-        //data null ise
-        if(snapshot.hasData){ //bos degilse dedik
-          return Container(
-            child: Center(
-              child: Text(
-                "gun : ${snapshot.data?.gun} \n + sabah : ${snapshot.data?.sabah} \n + ogle : ${snapshot.data?.ogle} \n + araogun : ${snapshot.data?.araogun} \n + aksam : ${snapshot.data?.aksam}"
-              ),
-            ),
-          );
-        } else {
-          return Center(child: CircularProgressIndicator(),);
-        }
-      },
-    )
+      body: FutureBuilder<LoadData>(
+        future: _loadDataFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.program1.length ?? 0,
+              itemBuilder: (context, index) {
+                var item = snapshot.data?.program1[index];
+                return Container(
+                  margin: const EdgeInsets.all(8),
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Gun: ${item?.gun}"),
+                      Text("Sabah: ${item?.sabah}"),
+                      Text("Ogle: ${item?.ogle}"),
+                      Text("Araogun: ${item?.araogun}"),
+                      Text("Aksam: ${item?.aksam}"),
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Hata: ${snapshot.error}'),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
--- lib/model/loadData.dart
+++ lib/model/loadData.dart
@@ -1,20 +1,96 @@
-class LoadData{
+// class LoadData{
+//   final String gun;
+//   final String sabah;
+//   final String ogle;
+//   final String araogun;
+//   final String aksam;
+
+//   LoadData({required this.gun, required this.sabah,required this.ogle,required this.araogun,required this.aksam});
+
+// /*
+//   factory LoadData.fromJson(Map<String, dynamic> json){
+//     return LoadData(
+//       gun: json["gun"] ?? "null kismi gun",
+//       sabah: json["sabah"] ?? "null kismi sabah",
+//       ogle: json["ogle"] ?? "null kismi ogle",
+//       araogun: json["araogun"] ?? "null kismi araogun",
+//       aksam: json["aksam"] ?? "null kismi aksam"
+//       );
+//   }
+// */
+// factory LoadData.fromJson(Map<String, dynamic> json) {
+//   if (json == null) {
+//     throw Exception("Geçersiz JSON verisi"); // JSON null ise hata fırlat
+//   }
+
+//   if (!json.containsKey("gun") ||
+//       !json.containsKey("sabah") ||
+//       !json.containsKey("ogle") ||
+//       !json.containsKey("araogun") ||
+//       !json.containsKey("aksam")) {
+//     throw Exception("Eksik veya hatalı alanlar"); // Gerekli alanlar eksik veya hatalı ise hata fırlat
+//   }
+
+//   return LoadData(
+//     gun: json["gun"] as String,
+//     sabah: json["sabah"] as String,
+//     ogle: json["ogle"] as String,
+//     araogun: json["araogun"] as String,
+//     aksam: json["aksam"] as String,
+//   );
+// }
+// }
+
+class LoadData {
+  final List<Program> program1;
+  final List<Program> program2;
+  final List<Program> program3;
+
+  LoadData({
+    required this.program1,
+    required this.program2,
+    required this.program3,
+  });
+
+  factory LoadData.fromJson(Map<String, dynamic> json) {
+    return LoadData(
+      program1: parseProgramList(json['program1']),
+      program2: parseProgramList(json['program2']),
+      program3: parseProgramList(json['program3']),
+    );
+  }
+
+  static List<Program> parseProgramList(dynamic json) {
+    if (json is List) {
+      return json.map((item) => Program.fromJson(item)).toList();
+    } else {
+      throw Exception("Program listesi dönüştürülürken hata oluştu");
+    }
+  }
+}
+
+class Program {
   final String gun;
   final String sabah;
   final String ogle;
   final String araogun;
   final String aksam;
 
-  LoadData({required this.gun, required this.sabah,required this.ogle,required this.araogun,required this.aksam});
+  Program({
+    required this.gun,
+    required this.sabah,
+    required this.ogle,
+    required this.araogun,
+    required this.aksam,
+  });
 
-  factory LoadData.fromJson(Map<String, dynamic> json){
-    return LoadData(
-      gun: json["gun"],
-      sabah: json["sabah"],
-      ogle: json["ogle"],
-      araogun: json["araogun"],
-      aksam: json["aksam"]
-      );
+  factory Program.fromJson(Map<String, dynamic> json) {
+    return Program(
+      gun: json['gun'] as String,
+      sabah: json['sabah'] as String,
+      ogle: json['ogle'] as String,
+      araogun: json['araogun'] as String,
+      aksam: json['aksam'] as String,
+    );
   }
-
 }
